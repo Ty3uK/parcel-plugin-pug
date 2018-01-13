7d5dff8ad6854b2b39441d52dc563c4b539491c4
Maksim Karelov <max.karelov@gmail.com>
2018-01-13 13:32:43 +0300

    fix: Dumb mistakes in `Asset/index.js`, added copying to build dir

--- src/Asset/index.js
+++ src/Asset/index.js
@@ -1,1 +1,1 @@
-export Asset = parseInt(process.versions.node, 10) < 8 ? require('parcel-bundler/lib/assets/JSAsset') : require('parcel-bundler/src/assets/JSAsset');
+module.exports.Asset = parseInt(process.versions.node, 10) < 8 ? require('parcel-bundler/lib/Asset') : require('parcel-bundler/src/Asset');
